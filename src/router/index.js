4ccab9525c4925d84675fd57760c11b38b091c9f
antonio.martino <antonio.martino.job@gmail.com>
2021-02-25 17:51:04 +0100

    feat(core): removed unused prop

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home,
-    props: route => ({ page: parseInt(route.query.page) || 1 })
+    component: Home
   },
   {
     path: '/events',
